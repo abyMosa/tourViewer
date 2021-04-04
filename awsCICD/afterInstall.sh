6a2cf89a0d7152684a26d14a6e87becce0bd84e3
A Badr <abdelrhman.badr@vodafone.com>
2021-04-04 23:35:00 +0100

    afterInstall

--- awsCICD/afterInstall.sh
+++ awsCICD/afterInstall.sh
@@ -1,6 +1,6 @@
 sudo rm -rf /var/www/rowilab/.htaccess
 sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]' >> /var/www/gprs/.htaccess
-sudo chown -R ubuntu /var/www/rowilab
+sudo chown -R ubuntu:ubuntu /var/www/rowilab
 cd /var/www/rowilab/server && npm install
 sudo systemctl restart nginx
 pm2 delete all
