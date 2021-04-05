fa1dd7177a3a72988685587fa1b101c0696f44be
A Badr <abdelrhman.badr@vodafone.com>
2021-04-05 19:39:26 +0100

    forceing https

--- awsCICD/afterInstall.sh
+++ awsCICD/afterInstall.sh
@@ -1,5 +1,5 @@
 sudo rm -rf /var/www/rowilab/.htaccess
-sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]' >> /var/www/gprs/.htaccess
+sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]'  'RewriteEngine On'  'RewriteCond %{HTTPS} off' 'RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]'  >> /var/www/gprs/.htaccess
 sudo chown -R ubuntu:ubuntu /var/www/rowilab
 sudo chown -R ubuntu:ubuntu /var/www/rowilabViewer
 cd /var/www/rowilab/server && npm install
