495a72c055e4ac83e4b4ddf7cab1565b93bca3e7
A Badr <abdelrhman.badr@vodafone.com>
2021-04-05 01:06:49 +0100

    build spec for viewer

--- awsCICD/afterInstall.sh
+++ awsCICD/afterInstall.sh
@@ -1,6 +1,7 @@
 sudo rm -rf /var/www/rowilab/.htaccess
 sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]' >> /var/www/gprs/.htaccess
 sudo chown -R ubuntu:ubuntu /var/www/rowilab
+sudo chown -R ubuntu:ubuntu /var/www/rowilabViewer
 cd /var/www/rowilab/server && npm install
 sudo systemctl restart nginx
 pm2 delete all
