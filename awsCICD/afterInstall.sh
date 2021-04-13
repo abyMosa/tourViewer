b20a5d288497e8d18440521888c33bf62ff83b4d
A Badr <abdelrhman.badr@vodafone.com>
2021-04-13 20:11:50 +0100

    upload to ec2 build 5

--- awsCICD/afterInstall.sh
+++ awsCICD/afterInstall.sh
@@ -1,10 +1,11 @@
-sudo rm -rf /var/www/rowilab/.htaccess
 # ONLY FOR apache servers
+# sudo rm -rf /var/www/rowilab/.htaccess
 # sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]'  'RewriteEngine On'  'RewriteCond %{HTTPS} off' 'RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]'  >> /var/www/rowilab/.htaccess
 # sudo printf '%s\n%s\n%s\n%s\n' 'Options -MultiViews' 'RewriteEngine On' 'RewriteCond %{REQUEST_FILENAME} !-f' 'RewriteRule ^ index.html [QSA,L]'  'RewriteEngine On'  'RewriteCond %{HTTPS} off' 'RewriteRule ^(.*)$ https://%{HTTP_HOST}%{REQUEST_URI} [L,R=301]'  >> /var/www/rowilabViewer/.htaccess
 sudo chown -R ubuntu:ubuntu /var/www/rowilab
 sudo chown -R ubuntu:ubuntu /var/www/rowilabViewer
-# cd /var/www/rowilab/server && npm install
+cd /var/www/rowilab/server && npm install
+cd /var/www/rowilab/server tsc
 sudo systemctl restart nginx
 pm2 delete all
 pm2 stop all
