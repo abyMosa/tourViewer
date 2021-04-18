b26f04ae356482a27bbb44fa34f666beb236dd1a
A Badr <abdelrhman.badr@vodafone.com>
2021-04-18 14:31:31 +0100

    viewer meta tags - label 2

--- viewer/metaDecorator.js
+++ viewer/metaDecorator.js
@@ -12,9 +12,14 @@
     addMetaTag([{ key: 'property', value: 'og:title' }, { key: 'content', value: label }]);
     addMetaTag([{ key: 'property', value: 'og:description' },]);
     addMetaTag([{ key: 'property', value: 'og:image' }, { key: 'content', value: imagePath }]);
+    addMetaTag([{ key: 'property', value: 'og:url' }, { key: 'content', value: window.location.href }]);
+
+    addMetaTag([{ key: 'property', value: 'og:site_name' }, { key: 'content', value: 'RowiLAB Viewer' }]);
+
     addMetaTag([{ key: 'name', value: 'twitter:title' }, { key: 'content', value: label }]);
     addMetaTag([{ key: 'name', value: 'og:description' },]);
     addMetaTag([{ key: 'name', value: 'twitter:image' }, { key: 'content', value: imagePath }]);
+    addMetaTag([{ key: 'name', value: 'twitter:card' }, { key: 'content', value: 'summary_large_image' }]);
     addMetaTag([{ key: 'name', value: 'image' }, { key: 'content', value: imagePath }]);
 
 
