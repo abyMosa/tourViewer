12b01128bcb8e8660d851f4bf8e0d12c1de661bc
A Badr <abdelrhman.badr@vodafone.com>
2021-04-20 01:33:13 +0100

    ignore __MACOSC 2

--- server/src/controllers/tourController.js
+++ server/src/controllers/tourController.js
@@ -161,12 +161,8 @@ const addTour = async (req, res) => {
     try {
         await decompress(req.body.filePath, unzipPath, {
             filter: file => {
-                console.log(file);
-                let fileAr = file.path.split('/');
-                fileAr = file.path.split("\\");
-                // return fileAr[0] !== '__MACOSX';
+                const fileAr = file.path.split("\\");
                 return !fileAr.includes('__MACOSX') || !file.path.startsWith('__MACOSX');
-                // return !fileAr.includes('__MACOSX');
             },
             strip: 1,
         });
