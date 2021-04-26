0d33293e85a48a44b8f614b473fb674e36277926
A Badr <abdelrhman.badr@vodafone.com>
2021-04-26 04:08:19 +0100

    tour description

--- server/src/models/Tour.js
+++ server/src/models/Tour.js
@@ -9,7 +9,7 @@ const mongoose = require('mongoose');
 
 const TourSchema = new mongoose.Schema({
     name: { type: String, },
-    description: { type: String, },
+    description: { type: String, default: '' },
     url: { type: String, required: true, },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true, },
     createdAt: { type: Date, default: Date.now }
